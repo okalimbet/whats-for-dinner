dc9b1f1e8ca555003274aa2e53fa869568ac7377
Olga Morgan <olga.morgan17@gmail.com>
2020-09-07 12:05:57 -0600

    Improve select tag by adding a drop-down menu

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 var letsCookButton = document.querySelector('#lets-cook-button')
+var clearRecipeButton = document.querySelector('#clear-button')
+var addRecipeButton = document.querySelector('#add-recipe-button')
 var showRandomDish = document.querySelector('.random-dish-name')
 var cookingPotView = document.querySelector('#cooking-pot')
 var recipeSectionView = document.querySelector('#recipe-section')
-var clearRecipeButton = document.querySelector('#clear-button')
-var addRecipeButton = document.querySelector('#add-recipe-button')
 var addARecipeBar = document.querySelector('#add-recipe-bar')
 var addNewButton = document.querySelector('#add-new')
 var recipeUserType = document.querySelector('#recipe-type')
 var recipeUserName = document.querySelector('#recipe-name')
+var recipeTypeList = document.querySelector("#recipe-type-select")
+
+var recipeTypeOptions = [
+  "--Choose your type--",
+  "Side",
+  "Main Dish",
+  "Dessert"
+]
 
 letsCookButton.addEventListener('click', getRandomDish)
 clearRecipeButton.addEventListener('click', clearRecipe)
 addRecipeButton.addEventListener('click', showAddRecipeBar)
 addNewButton.addEventListener('click', recordUserNewRecipe)
+
+function fillRecipeTypeList() {
+  for (var i = 0; i < recipeTypeOptions.length; i++) {
+    var recipeTypeOption = new Option (recipeTypeOptions[i])
+    recipeTypeList.options.add(recipeTypeOption)
+  }
+}
+
+fillRecipeTypeList()
 
 function getRandomDishValue(dishType) {
   var randomDishValue = Math.floor(Math.random() * dishType.length)
@@ -27,7 +44,7 @@ function getRandomDish() {
 
   cookingPotView.style.display = "none"
   recipeSectionView.style.display = "flex"
-
+  
   if (sideDishChecked) {
     showRandomDish.innerText = `${getRandomDishValue(sides)}!`
   }
@@ -48,6 +65,10 @@ function getRandomDish() {
 }
 
 function clearRecipe() {
+  document.querySelector('#sideDish').checked = false
+  document.querySelector('#mainDish').checked = false
+  document.querySelector('#dessert').checked = false
+  document.querySelector('#entireMeal').checked = false
   cookingPotView.style.display = "flex"
   recipeSectionView.style.display = "none"
   showRandomDish.innerText = ` `
@@ -57,24 +78,23 @@ function showAddRecipeBar() {
   addARecipeBar.style.display = addARecipeBar.style.display  ===  "flex" ? "none" : "flex"
   if (addARecipeBar.style.display === "none") {
     recipeUserName.value = " "
-    recipeUserType.value = " "
   }
-
 }
 
 function recordUserNewRecipe() {
-if (recipeUserType.value.toLowerCase() === "main") {
+  var recipeTypeSelected = recipeTypeList.options[recipeTypeList.selectedIndex].text;
+
+  if (recipeUserName.value == "" || recipeTypeSelected === "--Choose your type--") {
+    alert("Please choose a recipe type and enter a dish name")
+  }
+  else if (recipeTypeSelected === "Main Dish") {
     mains.push(recipeUserName.value)
-}
-else if (recipeUserType.value.toLowerCase() === "side") {
-  sides.push(recipeUserName.value)
-}
-else if (recipeUserType.value.toLowerCase() === "dessert") {
-  desserts.push(recipeUserName.value)
-}
-else if (recipeUserType.value == "" || recipeUserName.value==""){
-  alert("Please enter recipe type from above and a dish name")
-}
-recipeUserType.value = ""
-recipeUserName.value = ""
+  }
+  else if (recipeTypeSelected === "Side") {
+    sides.push(recipeUserName.value)
+  }
+  else if (recipeTypeSelected === "Dessert") {
+    desserts.push(recipeUserName.value)
+  }
+  recipeUserName.value = ""
 }
