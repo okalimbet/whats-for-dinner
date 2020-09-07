552014187c7a942cf25261d066ad119eebe36a40
Olga Morgan <olga.morgan17@gmail.com>
2020-09-07 09:13:07 -0600

    Create a seperate file to hold recipe data

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-var sides = ["Miso Glazed Carrots", "Coleslaw", "Garden Salad", "Crispy Potatoes", "Sweet Potato Tots", "Coconut Rice", "Caeser Salad", "Shrimp Summer Rolls", "Garlic Butter Mushrooms", "Hush Puppies"]
-var mains = ["Spaghetti and Meatballs", "Pineapple Chicken", "Shakshuka", "Thai Yellow Curry", "Bibimbap", "Chicken Parmesean", "Butternut Squash Soup", "BBQ Chicken Burgers", "Ramen", "Empanadas", "Chicken Fried Rice", "Sheet Pan Fajitas", "Margarita Pizza"]
-var desserts = ["Apple pie", "Lemon Meringue Pie", "Black Forest Cake", "Banana Bread", "Peach Cobbler", "Cheesecake", "Funfetti Cake", "Baklava", "Flan", "Macarons", "Chocolate Cupcakes", "Pavlova", "Pumpkin Pie", "Key Lime Pie", "Tart Tatin", "Croissants", "Eclair"]
-
 var letsCookButton = document.querySelector('#lets-cook-button')
 var showRandomDish = document.querySelector('.random-dish-name')
 var cookingPotView = document.querySelector('#cooking-pot')
